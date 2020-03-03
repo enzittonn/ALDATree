69b02fe0f120dff7764311e27f66785766a5142e
Enzo <45521416+enzittonn@users.noreply.github.com>
2020-03-03 13:52:24 +0100

    Update BinarySearchTreeNode.java

--- src/main/java/BinarySearchTreeNode.java
+++ src/main/java/BinarySearchTreeNode.java
@@ -1,31 +1,5 @@
-// Nyamgarig Naranbaatar nyna2000
-
-/**
- * Detta är den enda av de tre klasserna ni ska göra några ändringar i. (Om ni
- * inte vill lägga till fler testfall.) Det är också den enda av klasserna ni
- * ska lämna in. Glöm inte att namn och användarnamn ska stå i en kommentar
- * högst upp, och att paketdeklarationen måste plockas bort vid inlämningen för
- * att koden ska gå igenom de automatiska testerna.
- * <p>
- * De ändringar som är tillåtna är begränsade av följande:
- * <ul>
- * <li>Ni får INTE byta namn på klassen.
- * <li>Ni får INTE lägga till några fler instansvariabler.
- * <li>Ni får INTE lägga till några statiska variabler.
- * <li>Ni får INTE använda några loopar någonstans. Detta gäller också alterntiv
- * till loopar, så som strömmar.
- * <li>Ni FÅR lägga till fler metoder, dessa ska då vara privata.
- *
- *
- *
- * <li>Ni får INTE låta NÅGON metod ta en parameter av typen
- * BinarySearchTreeNode. Enbart den generiska typen (T eller vad ni väljer att
- * kalla den), String, StringBuilder, StringBuffer, samt primitiva typer är
- * tillåtna.
- * </ul>
- *
- * @param <T>
- * @author henrikbe
+/*
+ * @author enzo Naranbaatar
  */
 
 
